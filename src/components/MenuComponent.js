13c57553cee3f7e44f8932a5d6051396fcb23342
Shikhar Goel <shikhargoel4195@gmail.com>
2020-07-17 20:01:52 +0530

    Added Componnent to MenuComponent

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Media} from 'reactstrap';
+import {Card, CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from 'reactstrap';
 class Menu extends Component{
     constructor(props)
     {
@@ -7,26 +8,24 @@ class Menu extends Component{
     }
     render(){
         const menu=this.props.dishes.map((dish) => {
-            return (
-              <div key={dish.id} className="col-12 mt-5">
-                <Media tag="li">
-                  <Media left middle>
-                      <Media object src={dish.image} alt={dish.name} />
-                  </Media>
-                  <Media body className="ml-5">
-                    <Media heading>{dish.name}</Media>
-                    <p>{dish.description}</p>
-                  </Media>
-                </Media>
-              </div>
-            );
-        });;
+          return(
+              <div key={dish.id} className="col-sm-5 mt-5">
+                <Card>
+                  <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                  
+              </Card>
+            </div>
+        );
+        
+        });
             return(
                         <div className="conainer">
-                            <div className="row">
-                                <Media list>
-                                    {menu}
-                                    </Media>
+          +                  <div className="row">
+                                {menu}
                                 </div>
                             </div>
 
