e3118271fe5b14c2126ed6919defe4d2e35cd9bf
Shikhar Goel <shikhargoel4195@gmail.com>
2020-07-18 14:59:50 +0530

    Added comments on click event in DishDetail Component

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,49 +1,66 @@
 import React,{Component} from 'react';
-import {Card,CardTitle,CardBody,CardText} from 'reactstrap';
+import {Card,CardImg,CardTitle,CardBody,CardText} from 'reactstrap';
 class DishDetail extends Component{
     constructor(props)
     {
         super(props);
         
     }
-     renderDish(dish){
-           if(dish!=null)
-            {
-                return(
+    renderDish(dish) {
+        if (dish != null)
+            return(
                 <Card>
-
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>
-                {dish.description}
-                </CardText>
-                    </Card>
-                );
-
-            }
-            
-            else
-            {
-                return(
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
                 <div></div>
-                );
-            }
+            );
+    }
+    renderComments(comments){
+            if(comments!=null)
+            {
+                const com= comments.map((comment) => {
+            return(
+             <div key={comment.id}>
+                 <ul class="list-unstyled">
+                 <li>{comment.comment}</li>
+                 <li>--{comment.author},{comment.date}</li>
+                  </ul>
+                  </div>
+                  
+        
+        
+            );});
+       return com;
+       }
+else{
+    return(
+        <div></div>
+    );
+}
     }
         render(){
-            const selected=this.props.selectedDish;
-            const dish=this.props.dishes;
-            return(
-                <div className="container">
+                    const selected=this.props.selectedDish;
+                    const comments=this.props.comments;
+                    return(
+                            <div className="container">
                             <div className="row">
-                                <div className="col-12 col-md-6">
-
-                                </div>
-                                <div className="col-12 col-md-5 m-1">
-
-                                </div>
-                    </div>
-                    </div>
-                );
+                            <div className="col-12 col-md-5 m-1">
+                                    {this.renderDish(selected)}
+                                    </div>
+                                    <div className="col-12 col-md-5">
+                                        <h4>Comments</h4>
+                                       {this.renderComments(comments)}
+                                        </div>
+                            </div>
+                            </div>
+                        );
                 }
-
             }
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,34 +1,22 @@
 import React, {Component} from 'react';
 import {Media} from 'reactstrap';
 import {Card, CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 class Menu extends Component{
     constructor(props)
     {
         super(props);
         this.state={
-          selectedDish:null
+          selectedDish:null,
+          comment:null
       }
     }
     onDishSelect(dish)
     {
       this.setState({selectedDish:dish});
+      this.setState({comment:dish.comments});
     }
-    renderDish(dish) {
-      if (dish != null)
-          return(
-              <Card>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-          );
-      else
-          return(
-              <div></div>
-          );
-  }
+   
 
     render(){
         const menu=this.props.dishes.map((dish) => {
@@ -50,9 +38,7 @@ class Menu extends Component{
                                 {menu}
                                 </div>
                                 <div className="row">
-                                  <div className="col-12 col-md-5 m-1">
-                                    {this.renderDish(this.state.selectedDish)}
-                                    </div>
+                                 <DishDetail selectedDish={this.state.selectedDish} comments={this.state.comment} />
                                   </div>
                             </div>
 
